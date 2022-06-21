0038fa4b9e803d67b938dbd6ef0245f073ad0461
Gabriel Marques <bielmarques922@gmail.com>
2022-06-21 10:58:30 -0300

    04-sixtaxe-variaveis-e-fluxo - Variaveis guardam valores
    Entendendo o fluxo e comportamento das variaveis

---
+++ 04-sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,15 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+		primeiro = 10;
+		
+		// Quandp vale o segundo? 
+		// O segundo  vale 5
+		
+		System.out.println(segundo);
+	}
+}
